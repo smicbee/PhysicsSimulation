ba42e84bfea84ed365dda8cf99d2b6fcdf36fcf7
micha <micha@MICHI-PC>
2024-10-20 11:52:27 +0200

    Projektdateien hinzufügen.

---
+++ RectangleCollission/RectangleExtensions.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module RectangleExtensions
+    <Extension()>
+    Function MidX(ByVal rect As Rectangle) As Integer
+        Return rect.Left + rect.Width / 2
+    End Function
+
+    <Extension()>
+    Function MidY(ByVal rect As Rectangle) As Integer
+        Return rect.Top + rect.Height / 2
+    End Function
+
+    <Extension()>
+    Function Center(ByVal rect As Rectangle) As Point
+        Return New Point(rect.MidX(), rect.MidY())
+    End Function
+
+    <Extension()>
+    Function MidX(ByVal rect As RectangleF) As Single
+        Return rect.Left + rect.Width / 2
+    End Function
+
+    <Extension()>
+    Function MidY(ByVal rect As RectangleF) As Single
+        Return rect.Top + rect.Height / 2
+    End Function
+
+    <Extension()>
+    Function Center(ByVal rect As RectangleF) As PointF
+        Return New PointF(rect.MidX(), rect.MidY())
+    End Function
+End Module
